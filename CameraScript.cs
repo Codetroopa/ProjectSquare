a0b79e7230163ceda4fa6f778c1e89cd7e13e67b
Ryan Stephens <ryan_stephens99@hotmail.com>
2016-01-15 23:01:48 -0500

    Adding all current Scripts

---
+++ CameraScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraScript : MonoBehaviour {
+
+	public float yOffset = 1f;
+	public float xOffset = 1f;
+	public Transform myTransform;
+	
+	// Use this for initialization
+	void Start () 
+	{
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+			transform.position = new Vector3(myTransform.position.x + xOffset, transform.position.y, transform.position.z);
+	}
+}
